acd059a68478775ca1d009605500fffe3ca11c1a
adminsuiteteam-ux <adminsuiteteam-ux@users.noreply.github.com>
2026-08-05 13:37:24 -0800

    feat: add interactive WhatsApp live chat widget

--- frontend/src/components/layout/PageLayout.tsx
+++ frontend/src/components/layout/PageLayout.tsx
@@ -1,6 +1,7 @@
 import { Outlet, useLocation } from 'react-router-dom'
 import Navbar from './Navbar'
 import Footer from './Footer'
+import WhatsAppWidget from '@/components/ui/WhatsAppWidget'
 import { useEffect } from 'react'
 
 export default function PageLayout() {
@@ -11,13 +12,15 @@ export default function PageLayout() {
   }, [pathname])
 
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen relative">
       <Navbar />
       <main className="flex-1">
         <Outlet />
       </main>
       <Footer />
+      <WhatsAppWidget />
     </div>
   )
 }
+
 
